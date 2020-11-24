884b2dae275c31df7a5d80ae8a4a40ca0cbfde21
ironlord08 <devindunn@gmail.com>
2020-11-24 14:59:30 -0600

    edit

--- lesson11/js/currentweather.js
+++ lesson11/js/currentweather.js
@@ -1,4 +1,14 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=2b7568d348474360f18370d64df7ac87";
+let apiURL = ""
+if (document.querySelector(".active").textContent == "Preston"){
+  apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=2b7568d348474360f18370d64df7ac87";
+}
+else if (document.querySelector(".active").textContent == "Soda Springs"){
+  apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=2b7568d348474360f18370d64df7ac87";
+}
+else if (document.querySelector(".active").textContent == "Fish Haven"){
+  apiURL = "https://api.openweathermap.org/data/2.5/weather?lat=42.0380399&lon=-111.4048681&units=imperial&appid=2b7568d348474360f18370d64df7ac87";
+}
+// const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=2b7568d348474360f18370d64df7ac87";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
@@ -8,5 +18,16 @@ fetch(apiURL)
     document.getElementById('temp').textContent = Math.round(jsObject.main.temp);
     document.getElementById('humidity').textContent = jsObject.main.humidity
     document.getElementById('windSpeed').textContent = jsObject.wind.speed
-    
+
+    // Windchill
+    let t = parseFloat(document.querySelector("#temp").textContent)
+    let s = parseFloat(document.querySelector("#windSpeed").textContent)
+    let f
+    if (t <= 50 && s >= 3) {
+      f = Math.round(35.74 + 0.6215 * t - 35.75 * (s ** 0.16) + 0.4275 * t * (s ** 0.16))
+    } else {
+      f = "N/A"
+    }
+    document.querySelector("#windChill").textContent = f
+
   });
--- lesson11/js/forecast.js
+++ lesson11/js/forecast.js
@@ -1,4 +1,12 @@
-const apiURLforecast = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=2b7568d348474360f18370d64df7ac87";
+let apiURLforecast = ""
+if (document.querySelector(".active").textContent == "Preston") {
+    apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=2b7568d348474360f18370d64df7ac87";
+} else if (document.querySelector(".active").textContent == "Soda Springs") {
+    apiURLforecast = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&appid=2b7568d348474360f18370d64df7ac87";
+} else if (document.querySelector(".active").textContent == "Fish Haven") {
+    apiURLforecast = "https://api.openweathermap.org/data/2.5/forecast?lat=42.0380399&lon=-111.4048681&units=imperial&appid=2b7568d348474360f18370d64df7ac87";
+}
+// const apiURLforecast = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=2b7568d348474360f18370d64df7ac87";
 fetch(apiURLforecast)
     .then((response) => response.json())
     .then((jsObject) => {
